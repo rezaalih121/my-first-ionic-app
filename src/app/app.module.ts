1ec83efd4089f7323709a9f1bea079cc2a6b4c77
RezaAli <reza-ali.irannejad@eesc54.fr>
2023-02-21 09:49:58 +0100

    header component added to a ionic page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
--- src/app/contact/tab3.module.ts
+++ src/app/contact/tab3.module.ts
@@ -6,6 +6,7 @@ import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
+import { HeaderComponent } from '../components/header/header.component';
 
 @NgModule({
   imports: [
@@ -16,6 +17,6 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     Tab3PageRoutingModule,
     ReactiveFormsModule,
   ],
-  declarations: [Tab3Page],
+  declarations: [Tab3Page, HeaderComponent],
 })
 export class Tab3PageModule {}
